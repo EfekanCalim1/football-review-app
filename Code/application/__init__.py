258bf8273310d66912837d968318431644459998
EfekanCalim1 <ecalim@qa.com>
2020-11-12 15:53:00 +0000

    second update

--- Code/application/__init__.py
+++ Code/application/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SECRET_KEY'] = '123456789'
 
 db = SQLAlchemy(app)
