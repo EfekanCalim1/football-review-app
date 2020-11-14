c2e70cd9ad04a7ac7e466acebc9b73016413ad4a
EfekanCalim1 <ecalim@qa.com>
2020-11-14 16:44:07 +0000

    removed non-nullable players_id

--- Code/application/models.py
+++ Code/application/models.py
@@ -10,4 +10,4 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rev = db.Column(db.String(255))
     rating = db.Column(db.Integer)
-    players_id = db.Column(db.Integer, db.ForeignKey('players.id'), nullable=False)
+    players_id = db.Column(db.Integer, db.ForeignKey('players.id'))
