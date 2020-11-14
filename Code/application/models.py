b917af1ebbbef41cfd2ce7718ca2d42ae712205e
EfekanCalim1 <ecalim@qa.com>
2020-11-14 21:44:21 +0000

    changed reviews table

--- Code/application/models.py
+++ Code/application/models.py
@@ -9,4 +9,4 @@ class Review(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     rev = db.Column(db.String(255))
     rating = db.Column(db.Integer)
-    players_id = db.Column(db.Integer, db.ForeignKey('players.id'))
+    players_id = db.Column(db.Integer, db.ForeignKey('players.id'), nullable=True)
