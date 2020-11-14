60b915c2de0f1d6b8ae160c6449b8bf79b6c49c1
EfekanCalim1 <ecalim@qa.com>
2020-11-14 15:44:07 +0000

    changed forms

--- Code/application/forms.py
+++ Code/application/forms.py
@@ -28,7 +28,7 @@ class PlayersForm(FlaskForm):
 
 class ReviewForm(FlaskForm):
     rating = SelectField('Rating',
-                rating=[
+                choices=[
                     ('Ten', '10'), ('Nine', '9'), ('Eight', '8'), ('Seven', '7'), ('Six', '6'), ('Five', '5'), ('Four', '4'), ('Three', '3'), ('Two', '2'), ('One', '1'), ('Zero', '0')])
     rev = StringField('Review',
                 validators=[
